69bd5c1f722a796341e452b906883af7b13ab4ab
Shug1984 <l13074a@icloud.com>
2021-07-26 22:44:42 +0900

    9th commit

--- closet/urls.py
+++ closet/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ClosetCreateClass, closetlistview
+from .views import ClosetCreate, closetlistview, closet_completeview, closetdetailview
 
 urlpatterns = [
-    path('contents/closet_create/', ClosetCreateClass.as_view(), name = 'closet_create'),
+    path('contents/closet_create/', ClosetCreate, name = 'closet_create'),
     path('contents/closet_list/', closetlistview, name = 'closet_list'),
-
+    path('contents/closet_complete/', closet_completeview, name = 'closet_complete'),
+    path('contents/closet_detail/<int:pk>/',closetdetailview, name = 'closet_detail' )
     ]
--- closet/views.py
+++ closet/views.py
@@ -1,20 +1,44 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from .models import Closet
-from .forms import ClosetForm, ClosetNewCreation
+from .forms import ClosetForm
 
-class ClosetCreateClass(CreateView):
-    template_name = 'contents/closet_create.html'
-    model = Closet
-    form = ClosetNewCreation
-    fields = ('data_id','item_type','season','occasion','item_name','item_color','item_brand','purchase_date','pricing','purchase_place','memo','favorite_level','item_importance')
-    success_url = reverse_lazy('index')
+@login_required
+def ClosetCreate(request):
+    if request.method == 'POST':
+        form = ClosetForm(request.POST)
+        if form.is_valid():
+            action = request.POST['action']
+            if action == 'input':
+                return render(request, 'contents/closet_create.html', {'form':form})
+            elif action == 'confirm':
+                return render(request, 'contents/closet_confirm.html', {'form':form})
+            else:
+                closet = form.save(commit=False)
+                closet.user_id = request.user
+                closet.save()
+                return redirect('closet_complete')
+    else:
+        form = ClosetForm()
+    return render(request,'contents/closet_create.html', {'form':form})
 
 
+@login_required
+def closet_completeview(request):
+    return render(request, 'contents/closet_complete.html')
+    
+@login_required
 def closetlistview(request):
-    object_list = Closet.objects.all()
-    return render(request, 'closet_list.html',{'object_list':object_list})
+    object_list = Closet.objects.filter(user_id = request.user)
+    return render(request, 'contents/closet_list.html',{'object_list':object_list})
+
+@login_required
+def closetdetailview(request, pk):
+    object_list = Closet.objects.filter(user_id = request.user, pk=pk)
+    return render(request, 'contents/closet_detail.html',{'object_list':object_list})
+
 
 
 # Create your views here.
